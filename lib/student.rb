d31c890ecc5112bf564dec5b61724a3542e80679
Jesse Wenger <wengerj@gmail.com>
2021-03-25 16:19:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,8 +38,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create(hash) 
-    
+  def self.create(student) 
+    self.new(student.id, student.name, student.grade)
   end
 end
 
